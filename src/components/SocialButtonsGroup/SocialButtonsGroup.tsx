a55866f0f51968dcc00d619239f0642ecf0c0c42
xyashino <szczypiorpan123@gmail.com>
2023-05-27 17:44:44 +0200

    Create SocialButtonsGroup component

---
+++ src/components/SocialButtonsGroup/SocialButtonsGroup.tsx
@@ -0,0 +1,17 @@
+import { SocialButton } from "../UI/SocialButton/SocialButton.tsx";
+
+export const SocialButtonsGroup = () => {
+  return (
+    <div className="w-full flex flex-col items-center">
+      <p className="text-sm m-3 font-ligth text-gray-600 pb-2 border-b-2 w-4/5 text-center">
+        or continue with these social profile
+      </p>
+      <div>
+        <SocialButton icon="mdi:google" />
+        <SocialButton icon="mdi:facebook" />
+        <SocialButton icon="mdi:github" />
+        <SocialButton icon="mdi:twitter" />
+      </div>
+    </div>
+  );
+};
