03aa2c96895652d39811f43524f85bd010f11bfc
xyashino <szczypiorpan123@gmail.com>
2023-05-27 14:59:20 +0200

    Create ValidationInput component encapsulating Input, aiding in dynamic form validation.

---
+++ src/components/UI/input/ValidationInput.tsx
@@ -0,0 +1,35 @@
+import { twMerge } from "tailwind-merge";
+import { Input } from "./Input.tsx";
+import { InputProps } from "./input-props.ts";
+
+interface Props extends InputProps {
+  hasError: boolean;
+  errorMessage: string;
+}
+
+export const ValidationInput = ({
+  className = "",
+  hasError,
+  errorMessage,
+  ...rest
+}: Props) => {
+  const errorClasses = "border-red-600 focus:border-red-600";
+  const successClasses = "focus:border-emerald-600";
+
+  const baseClasses = twMerge(
+    hasError ? errorClasses : successClasses,
+    className
+  );
+
+  return (
+    <>
+      <Input {...rest} className={baseClasses}>
+        {hasError && (
+          <p className="relative w-full text-center text-red-500 font-bold uppercase text-sm -z-10">
+            {errorMessage}
+          </p>
+        )}
+      </Input>
+    </>
+  );
+};
