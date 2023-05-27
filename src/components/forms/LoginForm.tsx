94b53fa54dc7fbc2d3ae44a218bfed40d80941ee
xyashino <szczypiorpan123@gmail.com>
2023-05-27 17:45:29 +0200

    Create LoginForm component

---
+++ src/components/forms/LoginForm.tsx
@@ -0,0 +1,33 @@
+import { ValidationInput } from "../UI/Input/ValidationInput.tsx";
+import { Button } from "../UI/Button/Button.tsx";
+import { SocialButtonsGroup } from "../SocialButtonsGroup/SocialButtonsGroup.tsx";
+
+export const LoginForm = () => {
+  return (
+    <form className="flex flex-col justify-center items-center">
+      <ValidationInput
+        icon="mdi:email"
+        label="Email:"
+        hasError={false}
+        errorMessage="test"
+        placeholder="example@mail.com"
+      />
+      <ValidationInput
+        icon="mdi:lock"
+        label="Password:"
+        hasError={true}
+        errorMessage="Error message"
+        placeholder="*********"
+      />
+      <Button className="w-4/5">LOGIN</Button>
+      <SocialButtonsGroup />
+
+      <p className="text-sm m-3 text-gray-700 pb-2 w-full text-center">
+        Don't have account yet?{" "}
+        <span className="font-bold ml-1 text-emerald-600 hover:underline cursor-pointer">
+          Register
+        </span>
+      </p>
+    </form>
+  );
+};
