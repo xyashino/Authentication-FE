43b0dd8b9758e8577d5d9066647306d3fbb84c72
xyashino <szczypiorpan123@gmail.com>
2023-05-28 14:48:57 +0200

    Add more options in Avatar for to make it more elastic

--- src/components/ui/Avatar/Avatar.tsx
+++ src/components/ui/Avatar/Avatar.tsx
@@ -1,17 +1,29 @@
-interface Props {
+import { HTMLAttributes, PropsWithChildren } from "react";
+import { twMerge } from "tailwind-merge";
+
+interface Props extends HTMLAttributes<HTMLImageElement>, PropsWithChildren {
   src?: string;
 }
 
-export const Avatar = ({ src }: Props) => {
+export const Avatar = ({ src, children, className, ...rest }: Props) => {
+  const imageClasses = twMerge(
+    "aspect-square w-8 mr-5 rounded",
+    className ?? ""
+  );
+
   return (
-    <img
-      src={
-        src ||
-        "https://cdn1.iconfinder.com/data/icons/random-115/24/person-512.png"
-      }
-      alt="avatar"
-      className="aspect-square w-8 mr-5 rounded"
-      draggable={false}
-    />
+    <div className="relative">
+      <img
+        src={
+          src ||
+          "https://cdn1.iconfinder.com/data/icons/random-115/24/person-512.png"
+        }
+        alt="avatar"
+        className={imageClasses}
+        draggable={false}
+        {...rest}
+      />
+      {children}
+    </div>
   );
 };
