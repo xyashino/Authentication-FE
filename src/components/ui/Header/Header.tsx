9db7aab67a835b989565422debb9a8129824dbfc
xyashino <szczypiorpan123@gmail.com>
2023-05-28 14:48:11 +0200

    Change color in header to more transparent color

--- src/components/ui/Header/Header.tsx
+++ src/components/ui/Header/Header.tsx
@@ -3,7 +3,7 @@ import { Dropdown } from "@components/Dropdown/Dropdown.tsx";
 
 export const Header = () => {
   return (
-    <header className="w-full bg-sky-50 top-0 left-0 flex px-4 shadow sticky items-center justify-between">
+    <header className="w-full bg-sky-50/70 top-0 left-0 flex px-4 shadow sticky items-center justify-between">
       <Logo />
       <Dropdown />
     </header>
