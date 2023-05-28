5ccceea3050631fcace48f55e1f2c608537683c3
xyashino <szczypiorpan123@gmail.com>
2023-05-28 14:49:34 +0200

    Update colors in AuthenticationCard to more transparent

--- src/components/layout/AuthenticationCard/AuthenticationCard.tsx
+++ src/components/layout/AuthenticationCard/AuthenticationCard.tsx
@@ -3,7 +3,7 @@ import { Logo } from "@ui/Logo/Logo.tsx";
 
 export const AuthenticationCard = ({ children }: PropsWithChildren) => {
   return (
-    <div className="min-w-96 h-auto bg-sky-50 rounded-xl shadow-xl p-4 flex flex-col justify-center items-center">
+    <div className="min-w-96 h-auto bg-sky-50/70 rounded-xl shadow-xl p-4 flex flex-col justify-center items-center">
       <Logo />
       {children}
     </div>
