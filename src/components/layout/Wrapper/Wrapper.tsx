681930888ede8b7365d4ef8363d9e9711d7b563c
xyashino <szczypiorpan123@gmail.com>
2023-05-28 14:47:27 +0200

    Update style in Wrapper components

--- src/components/layout/Wrapper/Wrapper.tsx
+++ src/components/layout/Wrapper/Wrapper.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from "react";
 
 export const Wrapper = ({ children }: PropsWithChildren) => {
   return (
-    <div className="w-screen h-screen flex flex-col justify-center items-center bg-gradient-to-br from-emerald-100 to-emerald-900 overflow-x-hidden overflow-y-auto">
+    <div className="max-w-screen min-w-screen min-h-screen flex flex-col justify-center items-center bg-gradient-to-br from-emerald-100 to-emerald-900 overflow-x-hidden overflow-y-auto">
       {children}
     </div>
   );
