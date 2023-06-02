cff4f1b8c46bbe4d42e68c1dd9e2d32900b3c36e
xyashino <szczypiorpan123@gmail.com>
2023-06-02 18:30:21 +0200

    Create 'AuthorInfo' component

---
+++ src/components/ui/AuthorInfo/AuthorInfo.tsx
@@ -0,0 +1,15 @@
+export const AuthorInfo = () => {
+  return (
+    <p className="text-white font-bolder text-sm">
+      Created by{" "}
+      <a
+        href="https://github.com/xyashino"
+        className="appearance-none font-bold hover:underline"
+        target="_blank"
+        rel="noreferrer"
+      >
+        xyashino
+      </a>
+    </p>
+  );
+};
