5bc53db2da0f86b67175b8768643cc5fbd1b419a
xyashino <szczypiorpan123@gmail.com>
2023-06-04 12:17:39 +0200

    Implement new logic of useValidationForm hook

--- src/components/forms/LoginForm.tsx
+++ src/components/forms/LoginForm.tsx
@@ -1,32 +1,64 @@
 import { ValidationInput } from "@ui/Input/ValidationInput.tsx";
 import { Button } from "@ui/Button/Button.tsx";
 import { SocialButtonsGroup } from "@components/SocialButtons/SocialButtonsGroup.tsx";
+import { useValidationForm } from "@hooks/useValidationForm";
+
+enum InputNames {
+  EMAIL = "email",
+  PASSWORD = "password",
+}
 export const LoginForm = () => {
+  const { onChange, formState, isFormValid } = useValidationForm([
+    {
+      uniqueName: InputNames.EMAIL,
+      errorPrefix: "Email",
+      defaultValue: "",
+      validationData: {
+        minLength: 5,
+        maxLength: 255,
+        specialChars: ["@", "."],
+      },
+      attributes: {
+        type: "email",
+        icon: "mdi:email",
+        placeholder: "example@domain.com",
+        label: "Email:",
+      },
+    },
+    {
+      uniqueName: InputNames.PASSWORD,
+      errorPrefix: "Password",
+      defaultValue: "",
+      validationData: {
+        minLength: 8,
+        maxLength: 255,
+      },
+      attributes: {
+        type: "password",
+        icon: "mdi:lock",
+        placeholder: "*********",
+        label: "Password:",
+      },
+    },
+  ]);
+
   return (
     <form className="flex flex-col items-center justify-center">
-      <ValidationInput
-        icon="mdi:email"
-        label="Email:"
-        hasError={false}
-        errorMessage="test"
-        placeholder="example@mail.com"
-      />
-      <ValidationInput
-        icon="mdi:lock"
-        label="Password:"
-        hasError={true}
-        errorMessage="Error message"
-        placeholder="*********"
-      />
-      <Button className="w-4/5">LOGIN</Button>
-      <SocialButtonsGroup />
+      {formState.map(({ error, attributes, uniqueName, value }) => (
+        <ValidationInput
+          key={uniqueName}
+          error={error}
+          value={value}
+          name={uniqueName}
+          onChange={onChange}
+          {...attributes}
+        />
+      ))}
 
-      <p className="m-3 w-full pb-2 text-center text-sm font-bold text-emerald-50/90">
-        Don't have account yet?{" "}
-        <span className="ml-1 cursor-pointer font-bold uppercase text-emerald-800 transition-all hover:underline">
-          Register
-        </span>
-      </p>
+      <Button className="w-4/5" disabled={isFormValid}>
+        LOGIN
+      </Button>
+      <SocialButtonsGroup />
     </form>
   );
 };
