87a41e79f80253fd1e88a239bd9adf7dd72051d8
xyashino <szczypiorpan123@gmail.com>
2023-06-04 12:15:31 +0200

    Create AuthenticationPrompt

---
+++ src/components/ui/AuthenticationPrompt/AuthenticationPrompt.tsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+import { PageRoute } from "@enums/page-route.enum.ts";
+
+interface Props {
+  question: string;
+  linkText: string;
+  linkTo: PageRoute;
+}
+
+export const AuthenticationPrompt = ({ linkText, linkTo, question }: Props) => {
+  return (
+    <p className="m-3 w-full pb-1 text-center text-sm font-bold text-emerald-50">
+      {question}
+      <Link
+        to={linkTo}
+        className="ml-1 cursor-pointer font-bold uppercase text-emerald-900 hover:underline"
+      >
+        {linkText}
+      </Link>
+    </p>
+  );
+};
