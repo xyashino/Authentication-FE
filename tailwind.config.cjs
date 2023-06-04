4078d8c680ce9fd5fe462d35da6dcdf548218661
xyashino <szczypiorpan123@gmail.com>
2023-06-04 13:09:44 +0200

    Implement animation for error message

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-        "./index.html",
-        "./src/**/*.{js,ts,jsx,tsx}",
-        "./src/**/**/*.{js,ts,jsx,tsx}",
-    ],
-    theme: {
-        extend: {},
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      keyframes: {
+        errorText: {
+          "0%": { transform: "scale(0) translateY(-50%)", opacity: "0" },
+          "100%": { transform: "scale(1) translateY(0)", opacity: "1" },
+        },
+      },
+      animation: {
+        "show-text": "errorText .2s ease-in-out forwards",
+      },
     },
-    plugins: [],
-}
+  },
+  plugins: [],
+};
