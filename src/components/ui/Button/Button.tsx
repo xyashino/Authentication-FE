01f2597141019396e41039ab65ef13b252a00369
xyashino <szczypiorpan123@gmail.com>
2023-06-04 12:17:05 +0200

    Add disabled prop in button and some style for it

--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -1,11 +1,13 @@
 import { twMerge } from "tailwind-merge";
 import { HTMLAttributes, PropsWithChildren, SyntheticEvent } from "react";
 
-interface Props extends PropsWithChildren, HTMLAttributes<HTMLButtonElement> {}
+interface Props extends PropsWithChildren, HTMLAttributes<HTMLButtonElement> {
+  disabled?: boolean;
+}
 
 export const Button = ({ children, onClick, className, ...rest }: Props) => {
   const buttonClasses = twMerge(
-    "py-1 px-6 rounded-md text-center hover:scale-110 bg-emerald-700 transition-transform m-2 text-white font-bold shadow",
+    "select-none py-1 px-6 rounded-md text-center hover:scale-110 bg-emerald-700 transition-transform m-2 text-white font-bold shadow disabled:cursor-not-allowed disabled:pointer-events-none disabled:bg-emerald-700/50 ",
     className
   );
 
