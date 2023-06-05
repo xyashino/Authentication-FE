1ce27a365c216d2c73175d6c4489d73cd10b7287
xyashino <szczypiorpan123@gmail.com>
2023-06-05 11:51:37 +0200

    Add some animations

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,13 +8,19 @@ export default {
   theme: {
     extend: {
       keyframes: {
-        errorText: {
+        showItem: {
           "0%": { transform: "scale(0) translateY(-50%)", opacity: "0" },
           "100%": { transform: "scale(1) translateY(0)", opacity: "1" },
         },
+        showModal: {
+          "0%": { scale: 0, opacity: 0},
+          "50%": { scale: 1.3, opacity: 1 },
+          "100%": { scale: 1, opacity: 1 },
+        },
       },
       animation: {
-        "show-text": "errorText .2s ease-in-out forwards",
+        "show-text": "showItem .2s ease-in-out forwards",
+        "show-modal": "showModal .3s ease-in-out forwards",
       },
     },
   },
