9984f71fa793fe6ea880a5803cf9d84f204d1ceb
xyashino <szczypiorpan123@gmail.com>
2023-06-05 11:50:39 +0200

    Update Dropdown logic

--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -12,23 +12,32 @@ export const Dropdown = () => {
   ) as UserResponse;
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   const menuRef = useRef<HTMLDivElement>(null);
-
   const dropdownToggleClasses = isDropdownOpen
     ? "opacity-100 scale-1"
     : "opacity-0 scale-0";
 
   useLayoutEffect(() => {
-    if (isDropdownOpen && menuRef.current) menuRef.current.focus();
-  }, [isDropdownOpen]);
+    const handleClickOutside = (event: MouseEvent) => {
+      if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+        setIsDropdownOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
 
   return (
     <div className="relative mr-8">
       <button
         className="mr-8 flex h-full w-full cursor-pointer items-center justify-around rounded-xl p-2 hover:bg-neutral-50/60"
-        onClick={() => setIsDropdownOpen(true)}
+        onClick={() => setIsDropdownOpen(!isDropdownOpen)}
       >
         <Avatar src={avatar ?? undefined} />
-        <h2>{fullName ?? "Unkwon"}</h2>
+        <h2>{fullName ?? "Unknown"}</h2>
         <Icon
           icon="material-symbols:add-home-work-outline-rounded"
           className="h-8 w-8 text-emerald-900"
@@ -37,7 +46,6 @@ export const Dropdown = () => {
       <div
         className={`absolute top-full left-0 mt-4 bg-sky-50 w-full rounded transition-all duration-300 ${dropdownToggleClasses}`}
         ref={menuRef}
-        onBlur={() => setIsDropdownOpen(false)}
         tabIndex={0}
       >
         <DropdownMenu />
