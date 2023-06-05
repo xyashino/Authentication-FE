06c773db57bdd32a823a3a6ed82783859f642bfa
xyashino <szczypiorpan123@gmail.com>
2023-06-05 18:44:45 +0200

    Move ChangeAvatar to ChangeAvatar directory

--- src/components/Modals/ChangeAvatar/ChangeAvatar.tsx
+++ src/components/Modals/ChangeAvatar/ChangeAvatar.tsx
@@ -1,4 +1,4 @@
-import { useRouteLoaderData } from "react-router-dom";
+import { useRevalidator, useRouteLoaderData } from "react-router-dom";
 import { PageRoute } from "@enums/page-route.enum.ts";
 import { UserResponse } from "@backendTypes";
 import { Avatar } from "@ui/Avatar/Avatar.tsx";
@@ -6,16 +6,32 @@ import { Input } from "@ui/Input/Input.tsx";
 import { SyntheticEvent, useState } from "react";
 import { UploadImage } from "@components/UploadImage/UploadImage.tsx";
 import { Button } from "@ui/Button/Button.tsx";
+import { changeAvatar } from "@components/Modals/ChangeAvatar/ChangeAvatar.helper.ts";
+import { toast } from "react-hot-toast";
 
 export const ChangeAvatar = () => {
-  const { avatar } = useRouteLoaderData(PageRoute.HOME) as UserResponse;
+  const { avatar, id } = useRouteLoaderData(PageRoute.HOME) as UserResponse;
+  const { revalidate } = useRevalidator();
 
-  const [image, setImage] = useState(avatar);
+  const [image, setImage] = useState<null | string | File>(avatar);
 
   const handleChange = (e: SyntheticEvent) => {
     e.preventDefault();
     const { value } = e.target as HTMLInputElement;
     setImage(value);
+  };
+
+  const handleSave = (e: SyntheticEvent) => {
+    e.preventDefault();
+    const avatarPromise = changeAvatar(id, image);
+    toast.promise(avatarPromise, {
+      loading: "Saving...",
+      success: () => {
+        revalidate();
+        return "Saved!";
+      },
+      error: (err) => `${err}`,
+    });
   };
 
   return (
@@ -29,7 +45,10 @@ export const ChangeAvatar = () => {
         <span className="text-emerald-700"> Add Image</span>
       </h2>
       <div className="min-w-1/4 px-16 select-none">
-        <Avatar className="w-52" src={image ?? undefined} />
+        <Avatar
+          className="w-52"
+          src={image instanceof File ? URL.createObjectURL(image) : image}
+        />
       </div>
 
       <Input
@@ -40,10 +59,12 @@ export const ChangeAvatar = () => {
         className="border-2 border-emerald-600 w-full"
         wrapperClassName="grow"
         onChange={handleChange}
-        value={image ?? ""}
+        value={(image instanceof File ? image.name : image) ?? ""}
       />
-      <UploadImage setUrl={setImage} />
-      <Button className="text-2xl uppercase px-12 py-2">Save</Button>
+      <UploadImage setImage={setImage} />
+      <Button className="text-2xl uppercase px-12 py-2" onClick={handleSave}>
+        Save
+      </Button>
     </div>
   );
 };
