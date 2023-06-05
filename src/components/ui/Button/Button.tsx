90554b08e4ce12c3170e7993f3a0e2215aa7474d
xyashino <szczypiorpan123@gmail.com>
2023-06-05 18:42:38 +0200

    Delete default preventDefault in Button.tsx

--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -1,23 +1,18 @@
 import { twMerge } from "tailwind-merge";
-import { HTMLAttributes, PropsWithChildren, SyntheticEvent } from "react";
+import { HTMLAttributes, PropsWithChildren } from "react";
 
 interface Props extends PropsWithChildren, HTMLAttributes<HTMLButtonElement> {
   disabled?: boolean;
 }
 
-export const Button = ({ children, onClick, className, ...rest }: Props) => {
+export const Button = ({ children, className, ...rest }: Props) => {
   const buttonClasses = twMerge(
     "select-none py-1 px-6 rounded-md text-center hover:scale-110 bg-emerald-700 transition-transform m-2 text-white font-bold shadow disabled:cursor-not-allowed disabled:pointer-events-none disabled:bg-emerald-700/50 ",
     className
   );
 
-  const handleClick = (e: SyntheticEvent) => {
-    e.preventDefault();
-    onClick && onClick(e as any);
-  };
-
   return (
-    <button className={buttonClasses} onClick={handleClick} {...rest}>
+    <button className={buttonClasses} {...rest}>
       {children}
     </button>
   );
