6cd195be29f45c494e9da028a40724f9ac657c10
xyashino <szczypiorpan123@gmail.com>
2023-06-06 13:47:33 +0200

    Update bio

--- src/utils/network/axios-setup.ts
+++ src/utils/network/axios-setup.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const AxiosSetup = axios.create({
-  baseURL: import.meta.env.VITE_API_URL ?? "http://localhost:3000",
+  baseURL: import.meta.env.VITE_BE_URL ?? "http://localhost:3000",
   headers: {
     "Content-type": "application/json",
   },
